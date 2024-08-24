74cf0ea621d6a710988c981141b540f957204648
mashuq0068 <mashuq0068@gmail.com>
2024-08-24 15:13:13 +0600

    change sidebar color

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,18 +1,26 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../Component/Sidebar/Sidebar";
 import Navbar from "../Component/Navbar/Navabar";
-import "../assets/styles/sidebar/index.css"
+import "../assets/styles/sidebar/index.css";
+import { useState } from "react";
 
 const MainLayout = () => {
+  const [open, setOpen] = useState(true);
   return (
-    <div className="  gap-2 w-100">
-      <div className="w-100 position-sticky z-3 top-0">
-      <Navbar/>
-       
+    <div className="d-flex">
+      <div className="">
+        <Sidebar setOpen={setOpen} open={open} />
       </div>
-      <div className="d-flex gap-2 w-100">
-         <Sidebar />
-        <Outlet />
+      <div className="flex-grow-1 d-flex flex-column">
+        <div className="w-100 position-sticky flex-1 z-2 top-0">
+          <Navbar />
+        </div>
+        <div
+         
+          className={` ${open && "ms-auto custom-outlet-size "} px-3 flex-grow-1 `}
+        >
+          <Outlet />
+        </div>
       </div>
     </div>
   );
