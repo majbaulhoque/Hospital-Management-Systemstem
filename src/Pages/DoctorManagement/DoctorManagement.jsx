e363a6378fad2544d4f8c25d70bd4dca92382f7d
Majba UL Hoque <majba9399@gmail.com>
2024-08-28 20:59:12 +0600

    created a doctor management form

--- src/Pages/DoctorManagement/DoctorManagement.jsx
+++ src/Pages/DoctorManagement/DoctorManagement.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import DatePicker from 'react-datepicker';
+
 import 'react-datepicker/dist/react-datepicker.css';
 import '../../assets/styles/Doctor Management/DoctorManagementForm.css';
-import { IoIosArrowDropdown } from "react-icons/io";
+import { Button } from 'react-bootstrap';
+// import { IoIosArrowDropdown } from "react-icons/io";
 
 const DoctorManagement = () => {
     const [selectedDate, setSelectedDate] = useState(null);
 
+
     return (
         <div className='container mx-lg-auto'>
             <h2 className="fw-bolder m-3">Personal Information:</h2>
-            <div className='mx-3 mx-md-5 '>
+            <div className='doctor-management-form mx-3 mx-md-5 border p-3 rounded'>
                 <div className="row">
                     <div className="col-12 col-md-6 col-lg-4">
                         <FloatingLabel
@@ -81,14 +84,15 @@ const DoctorManagement = () => {
                 <div className="row">
                     <div className="col-12 col-md-6 col-lg-4 date-container">
                         <div className="date-wrapper">
-                            <p className='date-title'>Date of Birth</p>
+                            {/* <p className='date-title'>Date of Birth</p> */}
                             <DatePicker
                                 selected={selectedDate}
                                 onChange={date => setSelectedDate(date)}
                                 dateFormat="dd/MM/yyyy"
-                                className='date-picker mb-3'
+                                className='date-picker mb-3 p-2'
                                 showYearDropdown
                                 scrollableMonthYearDropdown
+                                placeholderText='Date of Birth'
                             />
                         </div>
                     </div>
@@ -142,8 +146,19 @@ const DoctorManagement = () => {
                 </div>
                 <div className="row">
                     <div className="col-12 col-md-4 gender-container">
-                        <input className='gender-input' />
-                        <IoIosArrowDropdown className='gender-icon h3' />
+                        <Form.Group className='mb-3 ' controlId="formBloodGroup">
+                            <Form.Select defaultValue="Choose..." className='blood-group'>
+                                <option>Blood Group</option>
+                                <option>A+</option>
+                                <option>A-</option>
+                                <option>B+</option>
+                                <option>B-</option>
+                                <option>AB+</option>
+                                <option>AB-</option>
+                                <option>O+</option>
+                                <option>O-</option>
+                            </Form.Select>
+                        </Form.Group>
                     </div>
                     <div className="col-12 col-md-4">
                         <FloatingLabel
@@ -156,14 +171,102 @@ const DoctorManagement = () => {
                     </div>
                     <div className="col-12 col-md-4">
                         <FloatingLabel
-                            controlId="floatingNumber"
-                            label="Contact Number"
+                            controlId="floatingGenderNumber"
+                            label="Gender Number"
                             className="mb-3"
                         >
-                            <Form.Control type="text" placeholder="Contact Number" />
+                            <Form.Control type="text" placeholder="Gender Number" />
                         </FloatingLabel>
                     </div>
                 </div>
+                <div className='row'>
+                    <div className="col-12 col-md-6 col-lg-7">
+                        <FloatingLabel
+                            controlId="floatingQualification"
+                            label="Qualification"
+                            className="mb-3"
+                        >
+                            <Form.Control type="text" placeholder="Qualification" />
+                        </FloatingLabel>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-5">
+                        <FloatingLabel
+                            controlId="floatingBankAccount"
+                            label="Bank Account"
+                            className="mb-3"
+                        >
+                            <Form.Control type="number" placeholder="Bank Account" />
+                        </FloatingLabel>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <FloatingLabel
+                            controlId="floatingDiscount"
+                            label="Discount"
+                            className="mb-3"
+                        >
+                            <Form.Control type="number" placeholder="Discount" required />
+                        </FloatingLabel>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <Form.Group className='mb-3' controlId="formDoctorArea">
+                            <Form.Select defaultValue="Doctor Area" className='blood-group'>
+                                <option>Doctor Area</option>
+                                <option>A+</option>
+                                <option>A-</option>
+                                <option>B+</option>
+                                <option>B-</option>
+                                <option>AB+</option>
+                                <option>AB-</option>
+                                <option>O+</option>
+                                <option>O-</option>
+                            </Form.Select>
+                        </Form.Group>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <FloatingLabel
+                            controlId="floatingRateOfVisit"
+                            label="Rate of Visit"
+                            className="mb-3"
+                        >
+                            <Form.Control type="text" placeholder="Rate of Visit" required />
+                        </FloatingLabel>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className="col-12 col-md-6 col-lg-6">
+                        <FloatingLabel
+                            controlId="floatingOb"
+                            label="OB"
+                            className="mb-3"
+                        >
+                            <Form.Control type="number" placeholder="OB" required />
+                        </FloatingLabel>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-6">
+                        <FloatingLabel
+                            controlId="floatingBalance"
+                            label="Balance"
+                            className="mb-3"
+                        >
+                            <Form.Control type="number" placeholder="Balance" />
+                        </FloatingLabel>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-6 col-lg-6 ">
+                        <div className='border'>
+
+                        </div>
+                    </div>
+                </div>
+                <Form.Group className="mb-3" id="formGridCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" required />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
             </div>
         </div>
     );
