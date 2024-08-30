66dee70c9645dc140b91b4d522477eade10800bb
Majba UL Hoque <majba9399@gmail.com>
2024-08-30 15:26:06 +0600

    added  input box & fixed responsive issue

--- src/Pages/LabTest/InvoiceGeneration.jsx
+++ src/Pages/LabTest/InvoiceGeneration.jsx
@@ -7,21 +7,34 @@ const InvoiceGeneration = () => {
             <div className="invoice-container border p-3 my-5 rounded-3 shadow-lg container">
                 <h2 className='text-center fw-bolder mb-5'>Invoice</h2>
                 <div className="row">
-                    <div className='d-flex justify-content-between'>
-                        <div className="col-12 col-md-6 col-lg-4">
-                            <Form.Group className="d-flex  mb-3" controlId="exampleForm.ControlInput1">
-                                <Form.Label className='fw-bold m-2'>Bill No : </Form.Label>
-                                <Form.Control type="number" placeholder="Bill ID" className='w-75 p-2' />
+                    <div className='d-flex justify-content-between mb-3'>
+                        <div className="col-12 col-md-6 col-lg-3">
+                            <Form.Group className="d-flex mb-3" controlId="billNumber">
+                                <Form.Label className='fw-bold m-2'>Bill No :</Form.Label>
+                                <Form.Control type="number" placeholder="Bill No" className='w-75 p-2' />
                             </Form.Group>
                         </div>
                         <div className="col-12 col-md-6 col-lg-3">
-                            <Form.Group className="d-flex  mb-3" controlId="exampleForm.ControlInput1">
-                                <Form.Label className='fw-bold m-2'>Date : </Form.Label>
+                            <Form.Group className="d-flex mb-3" controlId="patientId">
+                                <Form.Label className='fw-bold m-2'>Patient ID :</Form.Label>
+                                <Form.Control type="number" placeholder="Patient ID" className='w-50 p-2' />
+                            </Form.Group>
+                        </div>
+                        <div className="col-12 col-md-6 col-lg-3">
+                            <Form.Group className="d-flex mb-3" controlId="sampleId">
+                                <Form.Label className='fw-bold m-2'>Sample ID :</Form.Label>
+                                <Form.Control type="number" placeholder="Sample ID" className='w-50 p-2' />
+                            </Form.Group>
+                        </div>
+                        <div className="col-12 col-md-6 col-lg-3">
+                            <Form.Group className="d-flex mb-3" controlId="billDate">
+                                <Form.Label className='fw-bold m-2'>Date :</Form.Label>
                                 <Form.Control type="date" className='w-75 p-2' />
                             </Form.Group>
                         </div>
                     </div>
-                    <div className='d-flex gap-2'>
+
+                    <div className='d-flex flex-column flex-md-row gap-2'>
                         <div className='border shadow col-12 col-md-6 px-2'>
                             <Form.Group className="d-flex flex-column flex-md-row my-3" controlId="exampleForm.ControlInput1">
                                 <Form.Label className='fw-bold m-2 col-12 col-md-4'>Patient{`'`}s Name:</Form.Label>
@@ -72,34 +85,32 @@ const InvoiceGeneration = () => {
                         </div>
 
                         <div className="col-12 col-md-6 col-lg-6 border rounded shadow p-3">
-                            <Form.Group className="d-flex " controlId="exampleForm.ControlInput1">
-                                <Form.Label className="fw-bold mb-2 m-2 col-3">Doc Title :</Form.Label>
-                                <Form.Control type="text" placeholder="Doc Title" className="w-50 p-2" />
+                            <Form.Group className="d-flex flex-column flex-md-row" controlId="exampleForm.ControlInput1">
+                                <Form.Label className="fw-bold mb-2 m-2 col-12 col-md-3">Doc Title :</Form.Label>
+                                <Form.Control type="text" placeholder="Doc Title" className="w-100 w-md-50 p-2" />
                             </Form.Group>
-                            <Form.Group className="d-flex  my-3" controlId="exampleForm.ControlInput2">
-                                <Form.Label className="fw-bold mb-2 m-2 col-3">RefId Doctor :</Form.Label>
-                                <Form.Control type="text" placeholder="RefId Doctor" className="w-50 p-2" />
+                            <Form.Group className="d-flex flex-column flex-md-row my-3" controlId="exampleForm.ControlInput2">
+                                <Form.Label className="fw-bold mb-2 m-2 col-12 col-md-3">RefId Doctor :</Form.Label>
+                                <Form.Control type="text" placeholder="RefId Doctor" className="w-100 w-md-50 p-2" />
                             </Form.Group>
-                            <Form.Group className="d-flex  my-3" controlId="exampleForm.ControlInput3">
-                                <Form.Label className="fw-bold mb-2 m-2 col-3">Qualification :</Form.Label>
-                                <Form.Control type="text" placeholder="Qualification" className="w-50 p-2" />
+                            <Form.Group className="d-flex flex-column flex-md-row my-3" controlId="exampleForm.ControlInput3">
+                                <Form.Label className="fw-bold mb-2 m-2 col-12 col-md-3">Qualification :</Form.Label>
+                                <Form.Control type="text" placeholder="Qualification" className="w-100 w-md-50 p-2" />
                             </Form.Group>
-                            <Form.Group className="d-flex  my-3" controlId="exampleForm.ControlInput4">
-                                <Form.Label className="fw-bold mb-2 m-2 col-3">Doc Code :</Form.Label>
-                                <Form.Control type="text" placeholder="Doc Code" className="w-50 p-2" />
+                            <Form.Group className="d-flex flex-column flex-md-row my-3" controlId="exampleForm.ControlInput4">
+                                <Form.Label className="fw-bold mb-2 m-2 col-12 col-md-3">Doc Code :</Form.Label>
+                                <Form.Control type="text" placeholder="Doc Code" className="w-100 w-md-50 p-2" />
                             </Form.Group>
-                            <Form.Group className="d-flex  my-3" controlId="exampleForm.ControlInput5">
-                                <Form.Label className="fw-bold mb-2 m-2 col-3">Address :</Form.Label>
-                                <Form.Control type="text" placeholder="Address" className="w-50 p-2" />
+                            <Form.Group className="d-flex flex-column flex-md-row my-3" controlId="exampleForm.ControlInput5">
+                                <Form.Label className="fw-bold mb-2 m-2 col-12 col-md-3">Address :</Form.Label>
+                                <Form.Control type="text" placeholder="Address" className="w-100 w-md-50 p-2" />
                             </Form.Group>
                         </div>
                     </div>
-
-
                 </div>
                 <div className=''>
-                    <div className='mt-3 shadow col-12 col-md-8 col-lg-8'>
-                        <Table striped bordered hover size="sm">
+                    <div className='mt-3 shadow col-12'>
+                        <Table striped bordered hover size="sm" className='shadow rounded-bottom'>
                             <thead>
                                 <tr className='common-table'>
                                     <th>SL.</th>
@@ -134,13 +145,7 @@ const InvoiceGeneration = () => {
                             </tbody>
                         </Table>
                     </div>
-                    <div className='col-12 col-md-6 col-lg-6 d-flex justify-content-start'>
-                        <button className='custom-bg-main text-white border-0 px-4 py-2 rounded-3 me-2'>Referrel</button>
-                        <button className='custom-bg-main text-white border-0 px-4 py-2 rounded-3'>Requisition</button>
-                    </div>
                 </div>
-
-
             </div>
         </div>
     );
